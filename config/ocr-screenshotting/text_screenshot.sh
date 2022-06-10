27ed15a1fcc0c0fc3438a33ff0637a568110af9e
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-06-10 17:08:42 +0200

    did stuff

--- config/ocr-screenshotting/text_screenshot.sh
+++ config/ocr-screenshotting/text_screenshot.sh
@@ -1,25 +1,17 @@
 #!/bin/sh
 
 langs=$(tesseract --list-langs | tail +2)
-first_menu="$langs exit"
-selection_menu1=$(echo $first_menu | rofi -sep " " -dmenu)
+menu="$langs exit"
+selection=$(echo $menu | rofi -sep " " -dmenu)
 
 tmp_img=`mktemp`
 trap "rm $tmp_img*" EXIT
 
+if $selection_menu1 == "exit" then; exit; fi 
 
-echo $selection_menu1
-
-case $selection_menu1 in
-	"eng"|"deu"|"spa"|"osd"|"fra")
-		tesseract_lang=$selection_menu1
-		flameshot gui -p $tmp_img.png -d 100
-		mogrify -modulate 100,0 -resize 400% $tmp_img.png
-		;;
-	"exit")
-		exit
-		;;
-esac
+tesseract_lang=$selection_menu1
+flameshot gui -p $tmp_img.png -d 100
+mogrify -modulate 100,0 -resize 400% $tmp_img.png
 
 tesseract $tmp_img.png $tmp_img &> /dev/null
 xclip -i $tmp_img.txt -selection clipboard
